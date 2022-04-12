1214a7f46c0c426c7c0ad6a3527c6ea8d1640f64
hassanaftab93 <hassanaftab.pk@gmail.com>
2022-04-12 13:36:31 +0500

    Initial Commit

---
+++ WhiteSpaceRemover.ps1
@@ -0,0 +1,10 @@
+﻿# Import the data from CSV file and assign it to variable
+$Csv = Import-Csv "C:\Users\$env:UserName\Desktop\file.csv"
+
+# Trim values in CSV file
+$Csv | Foreach-Object {
+    $_.PSObject.Properties | Foreach-Object { $_.Value = $_.Value.Trim() }  
+}
+
+# Export CSV
+$Csv | Export-Csv C:\Users\$env:UserName\Desktop\file-new.csv -NoTypeInformation
